cd3b7ff1fbf41c000638ded16dd88bf01a391ccb
LeoPlugged <leandro.miguel@outlook.com>
2022-04-06 16:39:42 -0300

    Adicionar atualizações do projeto, ainda que incompleto.

---
+++ src/testesSimCity.java
@@ -0,0 +1,43 @@
+//layout e arquivo de teste
+/*
+::           MENU INICIAL           ::\n
+--------------------------------------\n
+|   INFORME DE IMPOSTOS DA SIMCITY   |\n
+|                                    |\n
+|Selecione uma das opções abaixo:    |\n
+|                                    |\n
+|[1] Cadastrar o(a) Sim              |\n
+|[2] Mostrar Cadastro do(a) Sim      |\n
+|[3] Rendimentos do Sim cadastrado   |\n
+|[4] Imposto de Rendimento do Sim    |\n
+|[5] Sair da o Programa              |\n
+|____________________________________|\n
+::           MENU INICIAL           ::\n
+\n"
+*/
+/*"
+:::::::::::::::::::::::::::::::::::::::::::::\n
+|         CADASTRAR O(A) SIM        |\n
+-------------------------------------\n
+\n
+Nome: "+nomeSim+"\n
+Cargo: "+profissaoSim+"\n
+\n
+Salário base:\n"
+
+"____________________________________\n
+:: Digite algo para voltar ao MENU INICIAL ::\n"
+*/
+
+/*
+:: Selecionado: '[5] Sair do Programa ::
+::            À Bientôt!             ::
+
+"::         CADASTRO DO(A) SIM         ::\n
+----------------------------------------\n
+Nome: "+nomeSim+"\n
+Cargo: "+profissaoSim+"\n
+. . . . . . . . . . . . . . . . . . . . \n
+::        Salário Base Mensal         ::\n
+----------------------------------------\n"
+*/
