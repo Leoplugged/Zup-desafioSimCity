d0301597072b45c47e8df00fe0d2ff12cf33261d
LeoPlugged <leandro.miguel@outlook.com>
2022-04-07 13:19:08 -0300

    Trocar o Switch por For, contando com colaboração de Adriano Konig

--- src/informeRendimento.java
+++ src/informeRendimento.java
@@ -2,67 +2,84 @@ import java.util.Scanner;
 
 public class informeRendimento {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-        
-        //## BASES ##
-        String[] cadastrarSim = new String[2];
-        String[] cadastroSim = new String[2];
-        double[] listaSalarios = new double[2];//mostrando apenas 2 meses para rápida testagem
-        double[] impostoRenda = new double[2];//mostrando apenas 2 meses para rápida testagem
+        rodarPrograma();
+    }
 
-        //## MENU ##
-        //Layout do Menu
-        
-        boolean selecaoValida = true;
-        
-        //Aviso de Seleções
-        String[] selecoesMenu = new String [5];
-        
-        selecoesMenu[0] = ":: Selecionado: '[1] Cadastrar o(a) Sim ::'\n";
-        selecoesMenu[1] = ":: Selecionado: '[2] Mostrar Cadastro do(a) Sim ::'\n";
-        selecoesMenu[2] = ":: Selecionado: '[3] Rendimentos do Sim cadastrado ::'\n";
-        selecoesMenu[3] = ":: Selecionado: '[4] Imposto de Rendimento do Sim ::'\n";
-        selecoesMenu[4] = ":: Selecionado: '[5] Sair do Programa' ::\n::             À Bientôt!              ::\n";
+    public static void rodarPrograma() {
+        Scanner leitorCad = new Scanner(System.in);
+        String nomeSim = "";
+        String profissaoSim = "";
+        Double[] listaSalarios = new Double[12];
+        for (int i = 0; i < 1000; i++) {     
+            System.out.println("\n\n::           MENU INICIAL           ::\n--------------------------------------\n|   INFORME DE IMPOSTOS DA SIMCITY   |\n|                                    |\n|Selecione uma das opções abaixo:    |\n|                                    |\n|[1] Cadastrar o(a) Sim              |\n|[2] Mostrar Cadastro do(a) Sim      |\n|[3] Rendimentos do Sim cadastrado   |\n|[4] Imposto de Rendimento do Sim    |\n|[5] Sair do Programa                |\n|____________________________________|\n::           MENU INICIAL           ::\n\n");          
+            int selecao = leitorCad.nextInt();
 
-        //Layout Cadastrar Sim
-        String layoutMenu = "\n\n::           MENU INICIAL           ::\n--------------------------------------\n|   INFORME DE IMPOSTOS DA SIMCITY   |\n|                                    |\n|Selecione uma das opções abaixo:    |\n|                                    |\n|[1] Cadastrar o(a) Sim              |\n|[2] Mostrar Cadastro do(a) Sim      |\n|[3] Rendimentos do Sim cadastrado   |\n|[4] Imposto de Rendimento do Sim    |\n|[5] Sair do Programa                |\n|____________________________________|\n::           MENU INICIAL           ::\n\n";
-        String layoutCad1 = ":::::::::::::::::::::::::::::::::::::::::::::\n|             CADASTRAR O(A) SIM            |\n---------------------------------------------\n\nNome e sobrenome: ";
-        String layoutCad2 = "Cargo: \n";     
-        String layoutCad3 = "_____________________________________________\n::                                           ::\n";
-        //String layout4 = "";  
-
-        System.out.println(layoutMenu);
-        int selecao = leitor.nextInt();
-
-        do while(selecaoValida == true) {     
-            switch(selecao){
-                case 1:{
-                    selecaoValida = true;
-                    System.out.println(selecoesMenu[0]+"\n"+layoutCad1);
-                    
-                    cadastrarSim(cadastrarSim);
-
-                    System.out.println(layoutCad3);
-                    System.out.println(layoutMenu);
-                    selecao = leitor.nextInt();
-                    break;
+            if(selecao == 1){
+                System.out.println("Nome Sim: ");
+                nomeSim = leitorCad.next();
+                System.out.println("Cargo do(a) Sim: ");
+                profissaoSim = leitorCad.next();
+                for (i = 0; i < listaSalarios.length; i++) {
+                    System.out.println("Mês "+(i+1)+": R$ ");
+                    listaSalarios[i] = leitorCad.nextDouble();
                 }
+            }
+            if(selecao == 2){
+                System.out.println("::         CADASTRO DO(A) SIM         ::\n----------------------------------------\nNome: "+nomeSim+"\nCargo: "+profissaoSim+"\n. . . . . . . . . . . . . . . . . . . . \n----------------------------------------\n");
+            
+            }
+            if(selecao == 3){
+                System.out.println("Mostrando Salários: ");
+                for (i = 0; i < listaSalarios.length; i++) {
+                    System.out.println("Mês "+(i+1)+": R$ "+listaSalarios[i]);
+                }
+            }
+            if(selecao == 4){
+                System.out.println("Imposto de Renda pago: ");
+                for (i = 0; i < listaSalarios.length; i++) {
+                    if(listaSalarios[i]<=2000.00) {
+                        System.out.println("Mês "+(i+1)+": R$"+0.00);
+                        
+                    }else if(listaSalarios[i] > 2000.00 && listaSalarios[i] <= 3000.00){
+                        System.out.println("Mês "+(i+1)+": R$"+listaSalarios[i]*0.08);
+
+                    }else if(listaSalarios[i] > 3000.00 && listaSalarios[i] <= 4500.00){
+                        System.out.println("Mês "+(i+1)+": R$"+listaSalarios[i]*0.18);
+                    }else{
+                        System.out.println("Mês "+(i+1)+": R$"+listaSalarios[i]*0.28);
+                    }
+                }
+            }
+            if(selecao == 5){
+                System.exit(0);
+            }
+
+                /*cadastroSim(cadastroSim);
+                System.out.println(selecoesMenu[1]);
+                
+                switch (selecao){
+                    case 1:{
+                    System.out.println(layoutMenu);
+                    }
+                    default:{
+                    System.out.println("Pressione 1 para prosseguir, por favor.");
+            
+            }else{
+                System.out.println(selecoesMenu[4]);
+                System.exit(0);
+
+            }
+            
+            }
+            /*   System.out.println(layoutMenu);
+                selecao = leitor.nextInt();
 
                 case 2:{
                     selecaoValida = true;
-                    System.out.println(selecoesMenu[1]);
                     
-                    cadastroSim(cadastroSim);
-
-                    selecao = leitor.nextInt();
-                    switch (selecao){
-                        case 1:{
-                        System.out.println(layoutMenu);
-                        }
-                        default:{
-                        System.out.println("Pressione 1 para prosseguir, por favor.");
                         }
                     }
+                    
                 }
                 case 3:{
                     selecaoValida = true;
@@ -84,8 +101,7 @@ public class informeRendimento {
             }
             case 5:{
                 selecaoValida = true;
-                System.out.println(selecoesMenu[4]);
-                System.exit(0);
+
             }
             default:{
                 selecaoValida = false;
@@ -93,56 +109,38 @@ public class informeRendimento {
                 System.out.println(selecoesMenu[4]);
                 System.out.println(layoutMenu);
                 selecao = leitor.nextInt();
-            }
+            }*/
         }
     }
     
-
+/*
     public static String[] cadastrarSim(String[] cadastrarSim) {
     //Cadastrar o(a) Sim
         Scanner leitorCad = new Scanner(System.in);
         int i = 0;
 
-        System.out.println("Nome do(a) Sim: ");
-        cadastrarSim[0] = leitorCad.nextLine();
         
-        System.out.println("Cargo do(a) Sim: ");
-        cadastrarSim[1] = leitorCad.nextLine();
 
         System.out.println("Salário-base mensal:");
         
-        for (i = 0; i < listaSalarios.length; i++) {
-            System.out.println("Mês "+(i+1)+": R$ ");
-            listaSalarios[i] = leitor.nextDouble();
+        
         }
 
         return cadastrarSim;
     }
     public static String[] cadastroSim(String[] cadastroSim) {
         //Cadastro do(a) Sim
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("::         CADASTRO DO(A) SIM         ::\n----------------------------------------\nNome: "+nomeSim+"\nCargo: "+profissaoSim+"\n. . . . . . . . . . . . . . . . . . . . \n----------------------------------------\n");
         System.out.println("::     Pressione 1 p/ prosseguir    ::");
     }
 
     /*
                     :: Modulo Salários Mensais ::
                     for (i = 0; i < listaSalarios.length; i++) {
-                    System.out.println("Mês "+(i+1)+": R$ "+listaSalarios[i]);
+                    
                 }
     */
     /*
                     :: Módulo Cálculo do IRSim ::                      
-                    if(listaSalarios[i]<=2000.00) {
-                    System.out.println("Mês "+(i+1)+": R$"+0.00);
                     
-                }else if(listaSalarios[i] > 2000.00 && listaSalarios[i] <= 3000.00){
-                    System.out.println("Mês "+(i+1)+": R$"+listaSalarios[i]*0.08);
-
-                }else if(listaSalarios[i] > 3000.00 && listaSalarios[i] <= 4500.00){
-                    System.out.println("Mês "+(i+1)+": R$"+listaSalarios[i]*0.18);
-                }else{
-                    System.out.println("Mês "+(i+1)+": R$"+listaSalarios[i]*0.28);
-                }
 */
 }
